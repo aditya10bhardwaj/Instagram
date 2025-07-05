4c940fdce1ed2ee9e6141f2e706f07a04f2abcc5
aditya10bhardwaj <138166827+aditya10bhardwaj@users.noreply.github.com>
2025-07-05 15:39:54 +0530

    Project Setup

---
+++ Instagram/App/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Aditya Bhardwaj on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
